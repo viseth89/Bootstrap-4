69c7046e32d781e44589b11960cfa99f9d4a5d4d
viseth89 <viseth89@gmail.com>
2019-01-22 11:02:06 -0600

    working on furniture

---
+++ codingchallenge.py
@@ -0,0 +1,25 @@
+# user_ids = [1,2,3,4,5,1]
+# line_counter = LineCounter(user_ids)
+# line_counter.count_users(1)
+# >>> 2
+
+x= 2
+y= 5
+
+function add(a,b):
+    c = ''
+    a+b = c
+    return c
+    
+add(x,y)
+
+# class LineCounter:
+#      def __init__()
+
+    #function(x,y)
+        x
+
+
+
+     # Create a function to count the numbers 
+     # 
