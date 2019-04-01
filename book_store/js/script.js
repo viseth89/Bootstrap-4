151342b0d9dae4420792faaf20cf0b2396029d39
viseth89 <viseth89@gmail.com>
2019-04-01 08:07:59 -0500

    added transition

--- book_store/js/script.js
+++ book_store/js/script.js
@@ -1,4 +1,19 @@
+$(document).ready(function() {
+  lightbox.option({
+    'wrapAround' : true
+  });
 
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    console.log(position);
+    if(position >= 350){
+      $('.gallery').addClass('change');
+    } else {
+      $('.gallery').removeClass('change');
+    }
+    
+  });
+});
 
 
 
